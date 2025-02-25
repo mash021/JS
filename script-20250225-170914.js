6bf8f780e5418d610f169f8630028279ffa0b6c7
Ali Sharifi <31005113+mash021@users.noreply.github.com>
2025-02-25 18:24:11 +0100

    Update script-20250225-170914.js

--- script-20250225-170914.js
+++ script-20250225-170914.js
@@ -1,1 +1,16 @@
 // Js commit 1 on Tue Feb 25 17:09:14 UTC 2025
+// Simple Calculator
+function calculator(a, b, operator) {
+    switch (operator) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return b !== 0 ? a / b : 'Cannot divide by zero';
+        default: return 'Invalid operator';
+    }
+}
+
+console.log(calculator(10, 5, '+'));
+console.log(calculator(10, 5, '-'));
+console.log(calculator(10, 5, '*'));
+console.log(calculator(10, 0, '/'));
