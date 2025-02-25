ffa5747979b9bdd24dff1cbac1102ca0c7dcd9bd
Ali Sharifi <31005113+mash021@users.noreply.github.com>
2025-02-25 18:24:33 +0100

    Update script-20250225-170945.js

--- script-20250225-170945.js
+++ script-20250225-170945.js
@@ -1,1 +1,11 @@
-// Js commit 2 on Tue Feb 25 17:09:45 UTC 2025
+// Random Password Generator
+function generatePassword(length) {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return password;
+}
+
+console.log("Generated Password:", generatePassword(12));// Js commit 2 on Tue Feb 25 17:09:45 UTC 2025
