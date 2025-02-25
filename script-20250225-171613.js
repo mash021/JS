0ad35649ec4a657c0c23d1a4373d0bfe1af92f4f
Ali Sharifi <31005113+mash021@users.noreply.github.com>
2025-02-25 18:25:26 +0100

    Update script-20250225-171613.js

--- script-20250225-171613.js
+++ script-20250225-171613.js
@@ -1,1 +1,7 @@
-// Js commit 2 on Tue Feb 25 17:16:13 UTC 2025
+// Check if a word is a palindrome
+function isPalindrome(word) {
+    return word === word.split('').reverse().join('');
+}
+
+console.log("Is 'racecar' a palindrome?", isPalindrome("racecar"));
+console.log("Is 'hello' a palindrome?", isPalindrome("hello"));// Js commit 2 on Tue Feb 25 17:16:13 UTC 2025
